fbdccd77a7f15187eb55dad00b5f12eab2b20b4b
Victor Yves Crispim <rotcivyves@hotmail.com>
2023-10-17 18:36:40 -0300

    run in no-backend mode

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { createApp } from "@deroll/app";
 import { decodeFunctionData, parseAbi } from "viem";
 
 // create application
-const app = createApp({ url: "http://127.0.0.1:5004" });
+const app = createApp({
+  url: process.env.ROLLUP_HTTP_SERVER_URL ?? "http://127.0.0.1:5004",
+});
 
 // define application ABI
 const abi = parseAbi([
